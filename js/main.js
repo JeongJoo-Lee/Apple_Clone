ccb6a0861a1d8ad76ed7bf97005fc5b5cf3a1548
JeongJoo-Lee <jjlee1623@gmail.com>
2021-06-16 23:33:34 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// 함수를 괄호로 감싸고 하면 바로 호출됨
+// 이렇게하는 이유는 전역에서 호출되는것을 막기위해
+(() => {
+  const sceneInfo = [
+    {
+      // 0
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      // 1
+      type: "normal",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      // 2
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      // 3
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+  }
+  window.addEventListener("resize", setLayout); // 창 크기 변할때마다(resize) 높이설정 변경실행(setLayout)
+  setLayout();
+})();
