c428f417026a8c34802bbdd854f0809370ec70f4
JeongJoo-Lee <jjlee1623@gmail.com>
2021-06-22 00:36:32 +0900

    씬 변경시 생기는 opacity 이산값(ex. -0.9788737173....) 발생 예방 코드 삽입 - enterNewScene

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
   let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스클로 섹션들의 스크롤 높이값의 합
   let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+  let enterNewScene = false; // 새로운 씬 시작되는 순간 true
 
   const sceneInfo = [
     {
@@ -73,40 +74,65 @@
     document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // 현재 scene에서 스크롤된 범위를 비율로 구하는 변수
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+
+    rv = scrollRatio * (values[1] - values[0]) + values[0];
+
+    return rv;
+  }
+
   function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
     switch (currentScene) {
       case 0:
-        console.log("0 play");
+        // console.log("0 play");
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+
         break;
 
       case 1:
-        console.log("1 play");
+        // console.log("1 play");
         break;
 
       case 2:
-        console.log("2 play");
+        // console.log("2 play");
         break;
 
       case 3:
-        console.log("3 play");
+        // console.log("3 play");
         break;
     }
   }
 
   function scrollLoop() {
     prevScrollHeight = 0;
+
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
       document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
     if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
       currentScene--;
       document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
+
+    if (enterNewScene) return;
 
     playAnimation();
   }
